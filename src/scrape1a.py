60419d4321beb013c06f69db7ee1a884233ce494
Stefan Griffin <griffinstef39@gmail.com>
2020-07-22 21:56:41 -0400

    First scrape

--- src/scrape1a.py
+++ src/scrape1a.py
@@ -5,8 +5,15 @@ my_url = input("Please enter the URL:   ")
 print("Grabbing...", my_url)
 
 response = requests.get(my_url)
+print("Status is:  ", response.status_code)  # 200, 404, 500
+if response.status_code != 200:
+    print("You cannot scrape!", response.status_code)
+else:
+    print("Scraping...")
+    print(response.text)
 
-print("Status is:  ", response.status_code)  # 200, 404
+
+
 
 
 
